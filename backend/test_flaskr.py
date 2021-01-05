2bbfc5b3a022e90f191b7dee44f92d9a057d463e
Andrew <gskll@protonmail.com>
2021-01-05 10:41:20 +0100

    test: test_get_all_categories

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -33,6 +33,13 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    def test_get_all_categories(self):
+        """Test GET endpoint to retrieve all categories"""
+        res = self.client.get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertTrue(len(data))
 
 
 # Make the tests conveniently executable
